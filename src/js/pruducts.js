b196385afcd51d15c1afc7f29c24d9a05e1e754d
Eduardo Rosales Alvarado <edrosrs99@gmail.com>
2025-07-25 08:05:11 -0600

    products updated

--- src/js/pruducts.js
+++ src/js/pruducts.js
@@ -1,22 +1,19 @@
-
 // Se obtiene el input
 const inputEl = document.getElementById("product-image");
 
 // Se agrega un evento y una función al input
 inputEl.addEventListener("change", () => {
-
-    // Se obtiene un archivo desde el input
+  // Se obtiene un archivo desde el input
   const file = inputEl.files[0];
 
   // Se crea un objeto FileReader
   const fr = new FileReader();
-  
-  // Se onfigura la salida de datos del FileReader como una URL string 
+
+  // Se onfigura la salida de datos del FileReader como una URL string
   fr.readAsDataURL(file);
 
   // Sees pera a que se complete la lectura del archivo
   fr.addEventListener("load", () => {
-
     // Se guarda el resultado
     const url = fr.result;
 
@@ -45,11 +42,26 @@ inputEl.addEventListener("change", () => {
 
 const getProducts = () => {
   const productos = document.getElementById("product-list");
-  const producto = document.createElement("h1");
 
-  let newProduct = localStorage.getItem("Product1");
-  producto.textContent = newProduct;
-  productos.append(producto);
+  for (i = 1; i <= localStorage.length; i++) {
+    let newProduct = localStorage.getItem(`Product${i}`);
+    let product = JSON.parse(newProduct);
+    let div = document.createElement("div");
+    let template = `<div class="card" style="width: 18rem">
+           <img src="${product.url}" class="card-img-top"/>
+           <div class="card-body d-grid text-center">
+             
+               <h4>${product.productName}</h4>
+               <strong>$ ${product.productCost}</strong>
+             
+             <p class="card-text text-start">
+               ${product.productDescription}
+             </p>
+           </div>
+         </div>`;
+    div.innerHTML = template;
+    productos.append(div);
+  }
 };
 
 window.onload = () => {
