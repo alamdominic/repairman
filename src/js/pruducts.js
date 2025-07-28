c707ff3889667bddd721b7c8d3483f78871fdf3b
EduRos21 <edrosrs99@gmail.com>
2025-07-28 12:15:04 -0600

    Update pruducts.js

--- src/js/pruducts.js
+++ src/js/pruducts.js
@@ -1,9 +1,69 @@
+// Se obtiene el input
+const inputEl = document.getElementById("product-image");
 
-const form = document.getElementById("add-Product");
-const onFormSumbmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+// Se agrega un evento y una función al input
+inputEl.addEventListener("change", () => {
+  // Se obtiene un archivo desde el input
+  const file = inputEl.files[0];
+
+  // Se crea un objeto FileReader
+  const fr = new FileReader();
+
+  // Se onfigura la salida de datos del FileReader como una URL string
+  fr.readAsDataURL(file);
+
+  // Sees pera a que se complete la lectura del archivo
+  fr.addEventListener("load", () => {
+    // Se guarda el resultado
+    const url = fr.result;
+
+    //Obtenemos el formulario del DOM
+    const form = document.getElementById("add-Product");
+
+    // Se crea un objeto FormData
+    const formData = new FormData(form);
+
+    // Agregamos un nuevo campo al formData
+    formData.append("url", url);
+
+    // Creación del objeto JS a partir del formData
     const newProduct = Object.fromEntries(formData.entries());
-    console.log(newProduct);
-}
-form.addEventListener("submit", onFormSumbmit);
+
+    // Se transforma en string para que sea posible almacenarlo en localstorage
+    const stringProduct = JSON.stringify(newProduct);
+
+    //Contador de productos (agrega un identificador al producto)
+    let productCounter = localStorage.length + 1;
+
+    let productKey = "Product" + productCounter;
+    localStorage.setItem(productKey, stringProduct);
+  });
+});
+
+const getProducts = () => {
+  const productos = document.getElementById("product-list");
+
+  for (i = 1; i <= localStorage.length; i++) {
+    let newProduct = localStorage.getItem(`Product${i}`);
+    let product = JSON.parse(newProduct);
+    let div = document.createElement("div");
+    let template = `<div class="card" style="width: 18rem">
+           <img src="${product.url}" class="card-img-top"/>
+           <div class="card-body d-grid text-center">
+             
+               <h4>${product.productName}</h4>
+               <strong>$ ${product.productCost}</strong>
+             
+             <p class="card-text text-start">
+               ${product.productDescription}
+             </p>
+           </div>
+         </div>`;
+    div.innerHTML = template;
+    productos.append(div);
+  }
+};
+
+window.onload = () => {
+  getProducts();
+};
